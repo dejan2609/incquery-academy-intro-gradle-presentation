034f82f9cfc6eeb1b0c9e19cf897e34b05fd2b77
Donat Csikos <donat@gradle.com>
2016-08-15 22:22:52 +0200

    Revert "third example"
    This reverts commit a71a8742c495928a33431085fb55100b374e5881.

---
+++ example-project/src/java/example/ClassyHello.java
@@ -0,0 +1,12 @@
+package example;
+
+public class ClassyHello {
+
+	public static void main(String[] args) {
+		System.out.println(getData());
+	}
+	
+	public static String getData() {
+		return "Hello, World";
+	}
+}
