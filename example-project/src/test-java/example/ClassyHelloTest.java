2789ac38ab2a4b77f311b411ff31608a1b7587e4
Donat Csikos <donat@gradle.com>
2016-08-16 20:41:41 +0200

    Revert "Fifth example"
    This reverts commit 8c4d9d402ce98a7f8bf119e18ea7bdb14244c08d.

--- example-project/src/test-java/example/ClassyHelloTest.java
+++ example-project/src/test-java/example/ClassyHelloTest.java
@@ -1,8 +1,14 @@
 package example;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ClassyHelloTest {
 
+	@Test
 	public void test() {
+		assertEquals("Hello, World", ClassyHello.getData());
 	}
 			
 }
