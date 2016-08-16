8c4d9d402ce98a7f8bf119e18ea7bdb14244c08d
Donat Csikos <donat@gradle.com>
2016-08-16 20:35:11 +0200

    Fifth example

--- example-project/src/test-java/example/ClassyHelloTest.java
+++ example-project/src/test-java/example/ClassyHelloTest.java
@@ -1,14 +1,8 @@
 package example;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 public class ClassyHelloTest {
 
-	@Test
 	public void test() {
-		assertEquals("Hello, World", ClassyHello.getData());
 	}
 			
 }
